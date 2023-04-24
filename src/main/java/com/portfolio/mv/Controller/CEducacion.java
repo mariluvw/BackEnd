aa3095e27c7036a78a57d1c58dbe8280d9c62aac
mariluvw <109830876+mariluvw@users.noreply.github.com>
2023-04-24 14:08:35 -0300

    deploy

--- src/main/java/com/portfolio/mv/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mv/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontend-95a85.web.app")
+@CrossOrigin(origins = {"https://frontend-95a85.web.app", "http://localhost:4200"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
